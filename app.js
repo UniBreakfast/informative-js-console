e4f2200412e7c4f17b8ad19e0c193d937153efc4
UniBreakfast <unibreakfast1@gmail.com>
2025-04-23 19:47:52 +0300

    allow cheating

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ class NamedArray extends Array {
   static create(name, ...pairs) {
     if (typeof name != 'string') pairs.unshift(name)
 
-    const arr = pairs.map(pair => NamedObject.create(...pair))
+    const arr = pairs.map(
+      pair => pair instanceof NamedObject || pair instanceof NamedArray 
+        ? pair : NamedObject.create(...pair)
+    )
 
     if (typeof name != 'string') return new NamedArray(...arr)
 
-    const Class = eval(`(class ${name} extends NamedArray {})`)
+    name = name.replace(/ /g, '_')
+      
+    if (doesNeedCheating(name)) name = cheat(name)
 
-    return new Class(...arr)
+    const Class = eval(`(class ${name.replace(/ /g, '_')} extends NamedArray {
+      ${arr.map((item, i) => `get ${item.getName()}() { return this[${i}] }`).join('\n')}
+    })`)
+
+    const namedArr = new Class(...arr)
+
+    for (const item of namedArr) {
+      try {
+        namedArr[item.getName()] = item
+      } catch {}
+    }
+    
+    return namedArr
+  }
+
+  getName() {
+    return NamedObject.prototype.getName.call(this)
   }
 
   getNames() {
@@ -18,7 +39,11 @@ class NamedArray extends Array {
 
 class NamedObject {
   static create(name, props) {
-    const Class = eval(`(class ${name} extends NamedObject {})`)
+    name = name.replace(/ /g, '_')
+      
+    if (doesNeedCheating(name)) name = cheat(name)
+
+    const Class = eval(`(class ${name.replace(/ /g, '_')} extends NamedObject {})`)
     const obj = new Class
 
     Object.assign(obj, props)
@@ -26,14 +51,13 @@ class NamedObject {
   }
 
   getName() {
-    return this.__proto__.constructor.name
-      .replace(/υ/g, 'u')
-      .replace(/с/g, 'c')
-      .replace(/о/g, 'o')
+    return uncheat(this.__proto__.constructor.name)
   }
 }
 
-const filingHolders = NamedArray.create('filing_holders',
+const cheats = {c: 'с', o: 'о', i: 'і', e: 'е', a: 'а', p: 'р', u: 'υ'}
+
+const filingHolders = NamedArray.create('filing holders',
   ['file', {}],
   ['folder', {}],
   ['repository', {}],
@@ -46,35 +70,79 @@ const filingHolders = NamedArray.create('filing_holders',
   ['documentation', {}],
 )
 
-const tokenCategories = NamedArray.create('token_categories',
-  ['keyword', {list: []}],
-  ['identifier', {list: []}],
-  ['operator', {list: []}],
-  ['literal', {list: []}],
-  ['punctuation', {list: []}],
-  ['comment', {list: []}],
-  ['whitespace', {list: []}],
+const tokenCategories = NamedArray.create('token categories',
+  ['keyword', { list: [] }],
+  ['identifier', { list: [] }],
+  ['operator', { list: [] }],
+  ['literal', { list: [] }],
+  ['punctuation', { list: [] }],
+  ['comment', { list: [] }],
+  ['whitespace', { list: [] }],
 )
 
-const dataTypes = NamedArray.create('data_types',
-  ['boolean', {сonstructor: Boolean}],
-  ['number', {сonstructor: Number}],
-  ['string', {сonstructor: String}],
-  ['undefined', {the_only_value: undefined}],
-  ['nυll', {the_only_value: null}],
-  ['bigint', {сonstructor: BigInt}],
-  ['symbol', {сonstructor: Symbol}],
-  ['object', {сonstructor: Object}],
+const statementCategories = NamedArray.create('statement categories',
+  ['declaration', { reference: 'declarationStatements' }],
+  ['control flow', { reference: 'controlFlowStatements' }],
+  ['expression', { reference: 'expressionCategories' }],
+  ['code block', { exampleLog: `` }],
+  ['error handling', { exampleLog: `` }],
+  ['empty', { exampleLog: `` }],
+  ['debugger', { exampleLog: `` }],
+  ['with', { exampleLog: `` }],
 )
 
-const bindingCategories = NamedArray.create('binding_categories',
-  ['constant', {exampleLog: ``}],
-  ['variable', {exampleLog: ``}],
-  ['functiоn', {exampleLog: ``}],
-  ['parameter', {exampleLog: ``}],
-  ['property', {exampleLog: ``}],
-  ['method', {exampleLog: ``}],
-  ['сlass', {exampleLog: ``}],
+const monoliterals = NamedArray.create('monoliterals',
+
+)
+
+const identifiers = NamedArray.create('identifiers',
+
+)
+
+const soloKeywords = NamedArray.create('solo keywords',
+
+)
+
+const structuredLiterals = NamedArray.create('structured literals',
+
+)
+
+const operatorExpressions = NamedArray.create('operator expressions',
+
+)
+
+const simpleExpressions = NamedArray.create('simple expressions',
+  ['monoliteral', { reference: monoliterals }],
+  ['identifier', { reference: identifiers }],
+  ['solo keyword', { reference: soloKeywords }],
+)
+
+const expressionCategories = NamedArray.create('expression categories',
+  ['simple expressions', { reference: simpleExpressions }],
+  ['structured literals', { reference: structuredLiterals }],
+  ['operator expressions', { reference: operatorExpressions }],
+  ['nested expressions', { exampleLog: `` }],
+)
+
+const dataTypes = NamedArray.create('data types',
+  ['boolean', { [cheat('constructor')]: Boolean }],
+  ['number', { [cheat('constructor')]: Number }],
+  ['string', { [cheat('constructor')]: String }],
+  ['undefined', { value: undefined }],
+  ['null', { value: null }],
+  ['bigint', { [cheat('constructor')]: BigInt }],
+  ['symbol', { [cheat('constructor')]: Symbol }],
+  ['object', { [cheat('constructor')]: Object }],
+)
+
+const bindingCategories = NamedArray.create('binding categories',
+  ['constant', { exampleLog: `` }],
+  ['variable', { exampleLog: `` }],
+  ['function', { exampleLog: `` }],
+  ['parameter', { exampleLog: `` }],
+  ['property', { exampleLog: `` }],
+  ['method', { exampleLog: `` }],
+  ['class', { exampleLog: `` }],
 )
 
 const interfaces = NamedArray.create('interfaces',
@@ -83,14 +151,42 @@ const interfaces = NamedArray.create('interfaces',
   ['API', {}],
 )
 
-const thinkingLevels = NamedArray.create('thinking_levels',
-  ['filing', {reference: filingHolders}],
-  ['code', {reference: tokenCategories}],
-  ['data', {reference: dataTypes}],
-  ['binding', {reference: bindingCategories}],
-  ['interfaсe', {reference: interfaces}],
+const codeSegments = NamedArray.create('code segments',
+  tokenCategories, statementCategories, expressionCategories
+)
+
+const thinkingLevels = NamedArray.create('thinking levels',
+  ['filing', { reference: filingHolders }],
+  ['code', { references: codeSegments }],
+  ['data', { reference: dataTypes }],
+  ['binding', { reference: bindingCategories }],
+  ['interface', { reference: interfaces }],
 )
 
 window.thinkingLevels = thinkingLevels
 
 console.log(thinkingLevels)
+
+function doesNeedCheating(name) {
+  try {
+    eval(`class ${name} {}`)
+    return false
+  } catch {
+    return true
+  }
+}
+
+function cheat(name) {
+  for (const key in cheats) {
+    name = name.replace(key, cheats[key])
+    if (!doesNeedCheating(name)) return name
+  }
+  throw new Error(`Can't cheat ${name}`)
+}
+
+function uncheat(name) {
+  for (const key in cheats) {
+    name = name.replace(cheats[key], key)
+  }
+  return name
+}
